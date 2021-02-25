49fa422e5e62432fc3e2fed2aebf930739793cd5
antonioChristofoletti <antoniochristofoletti123@gmail.com>
2021-02-24 22:21:23 -0300

    Studies from last weeks.

--- 04-nodejs-jwt-autenticacao-tokens/src/usuarios/usuarios-rotas.js
+++ 04-nodejs-jwt-autenticacao-tokens/src/usuarios/usuarios-rotas.js
@@ -2,17 +2,15 @@ const usuariosControlador = require('./usuarios-controlador')
 const middlewareAutenticacao = require('./middleware-autenticacao')
 
 module.exports = app => {
-
   app
     .route("/usuario/login")
     .post(
       middlewareAutenticacao.local,
       usuariosControlador.login)
-
+      
   app
     .route('/usuario')
-    .post(
-      usuariosControlador.adiciona)
+    .post(usuariosControlador.adiciona)
     .get(usuariosControlador.lista);
 
   app.route('/usuario/:id').delete(
